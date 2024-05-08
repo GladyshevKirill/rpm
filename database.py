ccd1a89bf3e39b8275f0fae47395d45ec9b87791
kashunya <156766930+kashunya@users.noreply.github.com>
2024-05-08 14:37:42 +0300

    массивы

--- database.py
+++ database.py
@@ -1,3 +1,13 @@
-with open('text.txt', 'r') as file:
-    read_file = file.read()
-print(read_file)
+a = int(input())
+b = input()
+if b == 'январь':
+    a = a+0
+elif b == 'февраль':
+    a = a+31
+
+list_ = []
+with open('holiday.txt', 'r', encoding='UTF-8') as file:
+    for line in file:
+        list_.append(line)
+
+print(list_[a])
