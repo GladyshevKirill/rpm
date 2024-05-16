da736a19a62882fbfc4b2b0634e6f34a926ee9d6
GladyshevKirill <gladysevk89@gmail.com>
2024-05-16 13:06:59 +0300

    ЗАРАБОТАЛО

--- inet.py
+++ inet.py
@@ -3,5 +3,15 @@ from bs4 import BeautifulSoup
 
 response = requests.get('https://www.rbc.ru/life/news/64888fdf9a79477a0585d069')
 
-print(response.text)
+with open('rbk.txt', 'w', encoding='utf-8') as file:
+    file.write(response.text)
+
+bs = BeautifulSoup(response.text, 'lxml')
+
+title = bs.find('p', 'paragraph')
+text_title = title.text
+
+word_selebrate = bs.find('ul', 'list circle gutter-b')
+word_selebrate_text = word_selebrate.text
+
 
