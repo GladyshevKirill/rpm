50d100d29e5a8645e54fc5a261f758180c8a4b82
kashunya <156766930+kashunya@users.noreply.github.com>
2024-05-16 17:24:30 +0300

    пара 16.05

--- inet.py
+++ inet.py
@@ -8,10 +8,50 @@ with open('rbk.txt', 'w', encoding='utf-8') as file:
 
 bs = BeautifulSoup(response.text, 'lxml')
 
-title = bs.find('p', 'paragraph')
-text_title = title.text
+#получение праздников paragraphs - это дофига текста
+selebrate_title = bs.findAll('h3', 'subtitle tertiary gutter-t')
+titles = str(selebrate_title)
+len_titles = titles.split('<h3 class="subtitle tertiary gutter-t">')
+titles = ''.join(len_titles)
+len_titles = titles.split('</h3>')
+titles = ''.join(len_titles)
+len_titles = titles.split(',')
 
+paragraph = bs.find_all('p', 'paragraph')
+paragraphs = str(paragraph)
+len_par = paragraphs.split('<p class="paragraph">')
+paragraphs = ''.join(len_par)
+len_par = paragraphs.split('</p>,')
+#print(len_par)
+
+
+#мировые праздники
+vstuplenie = len_par[0]
+vstuplenie = vstuplenie.replace('[', '')
+print(vstuplenie)
+a = len_titles[0]
+a = a.replace('[', '')
+print(a, ':')
 word_selebrate = bs.find('ul', 'list circle gutter-b')
-word_selebrate_text = word_selebrate.text
+word_selebrate = word_selebrate.text
+print(word_selebrate)
 
+#православный праздник
+b = len_titles[1]
+print(b, ':')
+print(len_par[1])
+
+#народный календарь
+c = len_titles[2]
+c = c.replace(']', '')
+print(c,':')
+narodni_kalendar = len_par[2]
+narodni_kalendar = narodni_kalendar.replace('</p>]', '')
+print(narodni_kalendar)
+
+
+#<h3 class="subtitle tertiary gutter-t"> - заголовки православных, народных и мировых праздников.
+#<h2 class="subtitle secondary"> - заголовки знаменитостей, памятные даты и именины
+#<p class="paragraph"> - православные, народные
+#ul class="list circle gutter-b" - международыне, знаменитости, даты в истории, именины
 
